3f943d4c68c2b63a62a830d89118d80b7738168c
Muneeba-Badar <122147598+Muneeba-Badar@users.noreply.github.com>
2024-02-10 15:13:34 +0500

    Add files via upload

--- PostScriptFileSimplifier.cpp
+++ PostScriptFileSimplifier.cpp
@@ -4,7 +4,7 @@ PostScriptFileSimplifier psfs;
 
 std::vector<std::string> PostScriptFileSimplifier::read_file(std::vector<std::string> store_file){
     std::ifstream myfile;
-    myfile.open("input\\file-1.ps"); //open the first file
+    myfile.open("input\\file-2.eps"); //open the first file
 
     while(myfile >> psfs.file_word){ //pushes into the vector till file ends
         store_file.push_back(psfs.file_word);
@@ -16,44 +16,94 @@ std::vector<std::string> PostScriptFileSimplifier::read_file(std::vector<std::st
 }
 
 void PostScriptFileSimplifier::simplifyFile(std::vector<std::string> store_file){
-    int num1, num2, ans;
-    std::cout<<num1<<" "<<num2<<" "<<ans;
+    int num1, num2, counter;
+    std::string lineToWrite;
+    
     // Create and open a text files
-    std::ofstream MyFile("test-output\\file-1-simplified.ps");
-    for (int i = 0; i < store_file.size(); i++){
-        if (isdigit(store_file[i][0]) == false && psfs.stack1.empty() == true){
-            MyFile << store_file[i] << " ";
+    std::ofstream MyFile("test-output\\file-2-simplified.eps");
+
+    for (int i = 0; i < store_file.size(); i++) //iterates over the entire vector which stores which stores each element of the file
+    {
+        if(isdigit(store_file[i][0])) //if the element is a digit push it into stack1
+        {
+            int x = std::stoi(store_file[i]); //converts the digit from string to int
+            psfs.stack1.push_back(x); //pushes into the stack
         }
-        else if (isdigit(store_file[i][0]) == true){
-            psfs.stack1.push_back(std::stoi(store_file[i]));
-            // std::cout<< " string " << store_file[i] << " num " << std::stoi(store_file[i]);
+        else //if element is not a digit
+        {
+            if (psfs.stack1.size() >= 2) //if more than 2 numbers in stack1
+            {
+                if(store_file[i] == "add")
+                {
+                    num1 = psfs.stack1.back();
+                    psfs.stack1.pop_back();
+                    num2 = psfs.stack1.back();
+                    psfs.stack1.pop_back();
+                    lineToWrite = std::to_string(num2+num1) + " ";
+                }
+                else if(store_file[i] == "sub")
+                {
+                    num1 = psfs.stack1.back();
+                    psfs.stack1.pop_back();
+                    num2 = psfs.stack1.back();
+                    psfs.stack1.pop_back();
+                    lineToWrite = std::to_string(num2-num1) + " ";
+                }
+                
+                else if(store_file[i] == "mul")
+                {
+                    num1 = psfs.stack1.back();
+                    psfs.stack1.pop_back();
+                    num2 = psfs.stack1.back();
+                    psfs.stack1.pop_back();
+                    lineToWrite = std::to_string(num2*num1) + " ";
+                }
+                else if(store_file[i] == "div")
+                {
+                    num1 = psfs.stack1.back();
+                    psfs.stack1.pop_back();
+                    num2 = psfs.stack1.back();
+                    psfs.stack1.pop_back();
+                    lineToWrite = std::to_string(num2/num1) + " ";
+                }
+                else
+                {
+                    num1 = psfs.stack1.back();
+                    psfs.stack1.pop_back();
+                    num2 = psfs.stack1.back();
+                    psfs.stack1.pop_back();
+                    lineToWrite =  std::to_string(num1) + " " + std::to_string(num2) + " " + store_file[i] + '\n';
+                }
+                MyFile << lineToWrite; //writes in the file
+                continue;
+            }
+            else if (psfs.stack1.size() == 1)
+            {
+                if(store_file[i] == "sqrt")
+                {
+                    num1 = psfs.stack1.back();
+                    psfs.stack1.pop_back();
+                    lineToWrite = std::to_string(sqrt(num1)) + " ";
+                }
+                else if(store_file[i] == "exp")
+                {
+                    num1 = psfs.stack1.back();
+                    psfs.stack1.pop_back();
+                    lineToWrite = std::to_string(exp(num1)) + " ";
+                }
+                else
+                {
+                    num1 = psfs.stack1.back();
+                    psfs.stack1.pop_back();
+                    lineToWrite =  std::to_string(num1) + " " + store_file[i] + '\n';
+                }
+                MyFile << lineToWrite; //writes in the file
+            }
+            else
+            {
+                MyFile << store_file[i] << '\n'; //writes in the file
+            }      
         }
-        else if (isdigit(store_file[i][0]) == false && stack1.empty() != false){
-            while(stack1.empty() == false){
-                num2 = psfs.stack1[psfs.stack1.size()];
-                psfs.stack1.pop_back();
-                num1 = psfs.stack1[psfs.stack1.size()];
-                psfs.stack1.pop_back();
-            }
-            if (store_file[i] == "add"){
-                ans = num1+num2;
-                MyFile << ans << " ";
-            }
-            else if (store_file[i] == "sub"){
-                ans = num1-num2;
-                MyFile << ans << " ";
-            }
-            else if (store_file[i] == "mul"){
-                ans = num1*num2;
-                MyFile << ans << " ";
-            }
-            else{
-                MyFile << num1 << " ";
-                MyFile << num2 << " ";
-                MyFile << store_file[i] << " ";
-            }
-        }  
     }
     MyFile.close();
 }
-
--- PostScriptFileSimplifier.hpp
+++ PostScriptFileSimplifier.hpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cmath>
 // #include "StackPostscript.hpp"
 
 
@@ -9,57 +10,12 @@ class PostScriptFileSimplifier{
     public:
         std::string file_word; //variable to store each word in file
         std::vector<std::string> store_file; //a vector to store what is being read in the file
-        std::vector<int> stack1; //stack for numbers
+        std::vector<int> stack1; //stack for numbers (Stores the numbers which are in the file)
 
         PostScriptFileSimplifier(){}; //constructor
         ~PostScriptFileSimplifier(){}; //destructor
 
         std::vector<std::string> read_file(std::vector<std::string> store_file); //function that reads the files
         void simplifyFile(std::vector<std::string> store_file);
-        int pop(std::vector<int> stack1);
+        // int pop(std::vector<int> stack1);
 };
-
-/*
-class PostScriptFileSimplifier{
-    public:
-        std::string file_word; //variable to store each word in file
-        std::vector<std::string> store_file; //a vector to store what is being read in the file
-
-        PostScriptFileSimplifier(){}; //constructor
-        ~PostScriptFileSimplifier(){}; //destructor
-
-        std::vector<std::string> read_file(std::vector<std::string> store_file){
-            std::ifstream myfile;
-            myfile.open("input\\file-1.ps"); //open the first file
-
-            while(myfile >> file_word){ //pushes into the vector till file ends
-            //     if (myfile >> '\n'){
-            //         std::cout<<"new lineee";
-            //     }
-            //     store_file.push_back(file_word);
-            // }
-            std::cout<<file_word;
-                if (file_word.back() == '\0') {
-                std::cout << "New line detected after word: " << file_word.substr(0, file_word.size() - 1) << std::endl;
-                }
-                
-                else {
-                    store_file.push_back(file_word);
-                }
-            }
-            myfile.close(); //close the file
-
-            return store_file;
-        }
-
-        void simplifyFile(std::vector<std::string> store_file){
-            // Create and open a text file
-            std::ofstream MyFile("test-output\\file-1-simplified.ps");
-            for (int i = 0; i < store_file.size(); i++){
-                // Write to the file
-                MyFile << store_file[i];
-            }
-            MyFile.close();
-        }
-};
-*/
